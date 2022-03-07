3c578a4f4d758454c998d2038997927df017dd03
baige369 <89885506+baige369@users.noreply.github.com>
2022-03-07 19:36:15 +0000

    Footer update

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,32 @@ export default function Footer() {
   return (
     <div className="Footer">
       <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/ewelinabaig/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ewelina Baig
+        </a>{" "}
+        and is
         <a
           href="https://github.com/baige369/weather-react"
           target="_blank"
           rel="noreferrer"
         >
-          Open-source code
+          {" "}
+          open-sourced on GitHub
         </a>{" "}
-        by Ewelina Baig
+        and
+        <a
+          href="https://loving-torvalds-5d25fb.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          hosted on Netlify
+        </a>
       </footer>
     </div>
   );
